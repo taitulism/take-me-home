713b67a7a1b1c784475c479074c2151fc3102544
Itai Tenenbaum <taitulism@gmail.com>
2017-02-24 03:11:46 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,11 @@ fi
 
 source ~/.bashrc
 
-echo "to install ag search run: $ sudo apt-get install silversearcher-ag"
-echo "Don't forget to enter your git user data in ~/.gitconfig"
+YELLOW=$'\033[1;33m'
+RED_BG=$'\e[31;7m'
+RST_CLR=$'\033[0m' # Reset Color
+
+echo ""
+echo -e "To install 'ag' search run: ${YELLOW}$ sudo apt-get install silversearcher-ag${RST_CLR}"
+echo ""
+echo -e "${RED_BG}Don't forget to enter your git user data in ~/.gitconfig${RST_CLR}"
