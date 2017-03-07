4c176daac9dd59f57b7e759e0ee454793690f056
Itai Tenenbaum <taitulism@gmail.com>
2017-03-07 22:17:24 +0200

    use van-gosh

--- take-me-home.sh
+++ take-me-home.sh
@@ -1,39 +1,27 @@
 #!/bin/bash
 
-# remember current take-me-home repo path
-# $path_to_take_me_home=$PWD
+# TMH = Take Me Home
+# remember current TMH repo path
+TMH_repo_path=$PWD
+home_TMH_path=$HOME/take-me-home
 
-# mkdir ~/take-me-home
-# cd ~/take-me-home
+mkdir "$home_TMH_path"
 
 # git clone z.sh
 # copy and rename z.sh stuff
-# git clone van-gosh
+
+# clone van-gosh
+git clone git@github.com:taitulism/van-gosh.git $home_TMH_path/van-gosh
 
 # cd van-gosh
-# source van-gosh
+echo $home_TMH_path/van-gosh/van-go.sh
+source $home_TMH_path/van-gosh/van-go.sh
 
-# cd back to take-me-home
-# cd $path_to_take_me_home
+logFail "success"
 
 # use van-gosh colorz and loggers
 # symlink vs-code stuff
 
-# colors
-# YELLOW=$'\e[1;33m'
-ORANGE=$'\e[0;33m'
-BLUE_BG=$'\e[34;7m'
-GREEN_BG=$'\e[32;7m'
-RED_BG=$'\e[31;7m'
-RST_CLR=$'\e[;0m' # Reset Color
-
-function log () {
-    echo "${ORANGE}* $1${RST_CLR}"
-}
-
-function warn () {
-    echo "${RED_BG}$1${RST_CLR}"
-}
 
 # ~/bashrc exists? prompt for backup
 if [ -e "~/.bashrc" ] ; then
@@ -41,47 +29,47 @@ if [ -e "~/.bashrc" ] ; then
 
     # backup current .bashrc
     if [ $bkpBashrc = "y" ] ; then
-        log "Backup ~/.bashrc"
+        logInfo "Backup ~/.bashrc"
         mv ~/.bashrc ~/.bashrc.bkp
     fi
 fi
 
 # create symlinks to .dotFiles
-log "Symlink .bashrc"
+logInfo "Symlink .bashrc"
 ln -sTf $PWD/home/.bashrc ~/.bashrc
 
-log "Symlink .aliases"
+logInfo "Symlink .aliases"
 ln -sTf $PWD/home/.aliases ~/.aliases
 
-log "Symlink .git-prompt"
+logInfo "Symlink .git-prompt"
 ln -sTf $PWD/home/.git-prompt ~/.git-prompt
 
-log "Symlink .inputrc"
+logInfo "Symlink .inputrc"
 ln -sTf $PWD/home/.inputrc ~/.inputrc
 
-log "Symlink .vimrc"
+logInfo "Symlink .vimrc"
 ln -sTf $PWD/home/.vimrc ~/.vimrc
 
-log "Symlink .z"
+logInfo "Symlink .z"
 ln -sTf $PWD/home/.z ~/.z
 
-log "Symlink .gitconfig"
+logInfo "Symlink .gitconfig"
 ln -sTf $PWD/home/.gitconfig ~/.gitconfig
 
 
 # copy vs-code stuff
 if [ -d ~/.config/Code/User ] ; then
-    log "Copy vs-code stuff"
+    logInfo "Copy vs-code stuff"
     cp -v -r ./vs-code/* ~/.config/Code/User
 else
-    warn "vs-code directory is not found: ~/.config/Code/User"
+    logWarn "vs-code directory is not found: ~/.config/Code/User"
 fi
 
-log "source new ~/.bashrc"
+logInfo "source new ~/.bashrc"
 source ~/.bashrc
 
 # ag
-log "sudo install ag (silversearcher)"
+logInfo "sudo install ag (silversearcher)"
 sudo apt-get install silversearcher-ag
 
-echo "${GREEN_BG}Done. Enjoy :]${RST_CLR}"
+logSuccess "Done. Enjoy :]"
