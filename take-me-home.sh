4932f96a0c1fcd2e5010b5145f8cdada30a92702
Itai Tenenbaum <taitulism@gmail.com>
2017-03-08 01:45:53 +0200

    backup by default: .bashrc & .gitconfig

--- take-me-home.sh
+++ take-me-home.sh
@@ -4,65 +4,70 @@
 # remember current TMH repo path
 TMH_repo_path=$PWD
 home_TMH_path=$HOME/take-me-home
+home_TMH_backups_path=$home_TMH_path/backups
 
 mkdir "$home_TMH_path"
+mkdir "$home_TMH_backups_path"
 
-logInfo "Clone van-gosh"
+logInfo "* Clone van-gosh"
 git clone git@github.com:taitulism/van-gosh.git $home_TMH_path/van-gosh
 
-logInfo "Clone z.sh"
+logInfo "* Clone z.sh"
 git clone git@github.com:rupa/z.git $home_TMH_path/z
 
-echo $home_TMH_path/van-gosh/van-go.sh
 source $home_TMH_path/van-gosh/van-go.sh
 
-# ~/bashrc exists? prompt for backup
-if [ -e "~/.bashrc" ] ; then
-    read -p "Backup ~/.bashrc? [y/n] " bkpBashrc
-
-    # backup current .bashrc
-    if [ $bkpBashrc = "y" ] ; then
-        logInfo "Backup ~/.bashrc"
-        mv ~/.bashrc ~/.bashrc.bkp
-    fi
+if [ -L ~/.bashrc ] ; then
+    backupFile .bashrc
+fi
+if [ -L ~/.gitconfig ] ; then
+    backupFile .gitconfig
 fi
 
+function backupFile () {
+    filename=$1
+
+    logInfo "* Backup ~/$filename"
+
+    mv ~/$1 $home_TMH_backups_path/$1.bkp
+}
+
 # create symlinks to .dotFiles
-logInfo "Symlink .bashrc"
+logInfo "* Symlink .bashrc"
 ln -sTf $PWD/home/.bashrc ~/.bashrc
 
-logInfo "Symlink .aliases"
+logInfo "* Symlink .aliases"
 ln -sTf $PWD/home/.aliases ~/.aliases
 
-logInfo "Symlink .git-prompt"
+logInfo "* Symlink .git-prompt"
 ln -sTf $PWD/home/.git-prompt ~/.git-prompt
 
-logInfo "Symlink .inputrc"
+logInfo "* Symlink .inputrc"
 ln -sTf $PWD/home/.inputrc ~/.inputrc
 
-logInfo "Symlink .vimrc"
+logInfo "* Symlink .vimrc"
 ln -sTf $PWD/home/.vimrc ~/.vimrc
 
-logInfo "Symlink .z"
+logInfo "* Symlink .z"
 ln -sTf $PWD/home/.z ~/.z
 
-logInfo "Symlink .gitconfig"
+logInfo "* Symlink .gitconfig"
 ln -sTf $PWD/home/.gitconfig ~/.gitconfig
 
 
 # copy vs-code stuff
 if [ -d ~/.config/Code/User ] ; then
-    logInfo "Copy vs-code stuff"
+    logInfo "* Copy vs-code stuff"
     cp -v -r ./vs-code/* ~/.config/Code/User
 else
     logWarn "vs-code directory is not found: ~/.config/Code/User"
 fi
 
-logInfo "source new ~/.bashrc"
+logInfo "* source new ~/.bashrc"
 source ~/.bashrc
 
 # ag
-logInfo "sudo install ag (silversearcher)"
+logInfo "* sudo install ag (silversearcher)"
 sudo apt-get install silversearcher-ag
 
 logSuccess "Done. Enjoy :]"
